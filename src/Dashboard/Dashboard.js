a57d865e3dc3aa481329e0673b201d8b3c57c34f
“Jose <claritooe@gmail.com>
2024-12-05 21:49:11 +0800

    fixed duplicate navigate import

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Dashboard.css'; 
 import CustomerList from '../CustomerList/CustomerList'; 
-import {useNavigate} from 'react-router-dom';
+
 
 import { Link } from 'react-router-dom'; 
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
-
-
 const Dashboard = ({ user, setUser }) => {
     const navigate = useNavigate(); // Initialize useNavigate for navigation
 
